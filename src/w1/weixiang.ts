7bce428f5162a1ec90277077934f2dbf8ca49958
weixiang <1294203675@qq.com>
2022-01-17 10:16:03 +0800

    完成w1,w2作业

---
+++ src/w1/weixiang.ts
@@ -0,0 +1,8 @@
+var _ = require('lodash');
+
+const genRandomNum = (j,k,n) => {
+    return _.chain(_.range(j, k)).shuffle().take(n).value();
+};
+
+const array = genRandomNum(3,8,3);
+console.log(array);
---
+++ src/w2/weixiang.ts
@@ -0,0 +1,15 @@
+var _ = require('lodash');
+
+const originData = [
+ {code:'a', b:'西瓜', c:'3'},
+ {code:'b', b:'苹果', c:'13'},
+ {code:'c', b:'葡萄', c:'23'},
+ {code:'d', b:'桃子', c:'33'},
+ {code:'e', b:'樱桃', c:'331'},
+ {code:'f', b:'哈密瓜', c:'43'},
+ ]
+
+// 解决方案1
+const data = _.chain(originData).keyBy('code').mapValues('b').value();
+// 解决方案2
+const data = _.reduce(originData, (pre, cur) => {pre[cur.code] = cur.b; return pre},{});
