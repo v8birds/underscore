94e6bd4ed4e339790aaf4796bf3edada04039a8b
柚柚 <92785047+yayaxiaoxiannv@users.noreply.github.com>
2022-03-05 01:45:26 +0800

    Update zhuziqiang.js

--- nodejs/src/w4/zhuziqiang.js
+++ nodejs/src/w4/zhuziqiang.js
@@ -2,11 +2,11 @@ const _ = require('lodash')
 const moment = require('moment')
 
 const toArrayByStartEndMonth = (start, end) => {
-    const a = moment(start)
-    const b = moment(end)
-    const startYear = _.map(_.range(a.month() + 1, 13), z => (a.year() + '-' + z))
-    const betweenYear = a.year() < b.year() ? _.chain(_.range(a.year() + 1, b.year())).map(a => _.map(_.range(1, 13), z => (a + '-' + z))).flatten().value() : []
-    const endYear = a.year() === b.year() ? [] : _.map(_.range(1, b.month() + 2), a => (b.year() + '-' + a))
+    const s = moment(start)
+    const e = moment(end)
+    const startYear = _.map(_.range(s.month() + 1, 13), z => (s.year() + '-' + z))
+    const betweenYear = s.year() < e.year() ? _.chain(_.range(s.year() + 1, e.year())).map(val => _.map(_.range(1, 13), z => (val + '-' + z))).flatten().value() : []
+    const endYear = s.year() === e.year() ? [] : _.map(_.range(1, e.month() + 2), val => (e.year() + '-' + val))
     console.log([...startYear, ...betweenYear, ...endYear])
 }
 toArrayByStartEndMonth('2019-10', '2020-12')
