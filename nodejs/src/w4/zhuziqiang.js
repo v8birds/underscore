43293a4012975d5699e1204f74fcd981e6d39e99
柚柚 <92785047+yayaxiaoxiannv@users.noreply.github.com>
2022-03-05 01:42:28 +0800

    Update zhuziqiang.js

--- nodejs/src/w4/zhuziqiang.js
+++ nodejs/src/w4/zhuziqiang.js
@@ -4,7 +4,7 @@ const moment = require('moment')
 const toArrayByStartEndMonth = (start, end) => {
     const a = moment(start)
     const b = moment(end)
-    const startYear = _.map(_.range(a.month() + 1, 12 + 1), z => (a.year() + '-' + z))
+    const startYear = _.map(_.range(a.month() + 1, 13), z => (a.year() + '-' + z))
     const betweenYear = a.year() < b.year() ? _.chain(_.range(a.year() + 1, b.year())).map(a => _.map(_.range(1, 13), z => (a + '-' + z))).flatten().value() : []
     const endYear = a.year() === b.year() ? [] : _.map(_.range(1, b.month() + 2), a => (b.year() + '-' + a))
     console.log([...startYear, ...betweenYear, ...endYear])
