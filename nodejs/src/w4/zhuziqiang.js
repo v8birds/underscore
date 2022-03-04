ee2a4c02a7217457c2f038a7e6855f72c2983ac7
柚柚 <92785047+yayaxiaoxiannv@users.noreply.github.com>
2022-03-05 01:03:28 +0800

    Add files via upload

---
+++ nodejs/src/w4/zhuziqiang.js
@@ -0,0 +1,12 @@
+const _ = require('lodash')
+const moment = require('moment')
+
+const toArrayByStartEndMonth = (start, end) => {
+    const a = moment(start)
+    const b = moment(end)
+    const startYear = _.map(_.range(a.month() + 1, 12 + 1), z => (a.year() + '-' + z))
+    const betweenYear = a.year() < b.year() ? _.chain(_.range(a.year() + 1, b.year())).map(a => _.map(_.range(1, 13), z => (a + '-' + z))).flatten().value() : []
+    const endYear = a.year() === b.year() ? [] : _.map(_.range(1, b.month() + 2), a => (b.year() + '-' + a))
+    console.log([...startYear, ...betweenYear, ...endYear])
+}
+toArrayByStartEndMonth('2019-10', '2020-12')
